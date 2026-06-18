8564ec3887c4da5f4f4de8c4c7bb50312b1374d4
Hazakura2024 <ytr240.jp@gmail.com>
2026-06-18 13:14:27 +0900

    feat: ヘッダーを追加

--- app/page.tsx
+++ app/page.tsx
@@ -6,21 +6,21 @@ import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/com
 export default function Home() {
   return (
     <>
-    {/* <header className="bg-blue-300 top-0 z-50 border-b   w-full">
+    <header className="fixed top-0 z-50 w-full border-b border-slate-200/50 bg-white/60 shadow-lg backdrop-blur-sm p-2">
       <a>
         <h1 className="text-xl font-bold">AI-news-summary</h1>
       </a>
-    </header> */}
-    <main className="flex min-h-screen flex-col items-center p-6  bg-[url('/bg-yokohama.jpg')] bg-cover bg-center bg-fixed">
+    </header>
+    <main className="flex min-h-screen flex-col items-center p-6 pt-24 bg-[url('/bg-yokohama.jpg')] bg-cover bg-center bg-fixed">
       <div className="w-full max-w-md space-y-8 bg-white/60 p-8 rounded-xl shadow-lg backdrop-blur-sm">
 
         <div className="w-full max-w-md space-y-8">
-          <h1 className="text-3xl font-bold ">タイトル</h1>
-          <p>説明</p>
+          <h1 className="text-3xl font-bold ">記事の要約を生成</h1>
+          <p>ネットニュースの記事のURLを入力すると、AIが3文で記事の要約を生成します。</p>
         </div>
 
-        <form className="flex flex-col gap-4 ">
-          <Input className="bg-background" placeholder="URLを入力..."/>
+        <form className="flex flex-col gap-4">
+          <Input className="bg-white" placeholder="URLを入力..."/>
           <Button className="font-bold">要約を生成</Button>
         </form>
 
